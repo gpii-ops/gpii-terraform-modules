b074f17d5571b396eaf6370610fd9d79846cffbe
Tyler Roscoe <tyler@cryptio.net>
2017-04-12 16:39:15 -0600

    Move values to defaults in variables.tf.
    This seems better (base doesn't freak out as a module) though the system still isn't working.

--- dev/base/terraform.tfvars
+++ dev/base/terraform.tfvars
@@ -3,9 +3,3 @@ terragrunt = {
     path = "${find_in_parent_folders()}"
   }
 }
-
-# Probably easiest to create a ~/.aws, as for use with the aws cli. However you
-# can also hardcode credentials here (for testing only -- don't commit them!).
-access_key = ""
-secret_key = ""
-region = "us-east-1"
--- dev/base/vars.tf
+++ dev/base/vars.tf
@@ -1,3 +1,12 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "region" {}
+# Probably easiest to create a ~/.aws, as for use with the aws cli. However you
+# can also hardcode credentials here (for testing only -- don't commit them!).
+variable "access_key" {
+  default = ""
+}
+variable "secret_key" {
+  default = ""
+}
+
+variable "region" {
+  default = "us-east-1"
+}
