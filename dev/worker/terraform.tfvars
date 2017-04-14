31514c2d53769b5fcf913d37d3805c7d74ad0092
Tyler Roscoe <tyler@cryptio.net>
2017-04-13 20:11:01 -0600

    Move variables from terraform.tfvars to vars.tf.
    This seems more flexible / clear in a modular world.

--- dev/worker/terraform.tfvars
+++ dev/worker/terraform.tfvars
@@ -7,15 +7,3 @@ terragrunt = {
     paths = ["../base"]
   }
 }
-
-key_name = "donkey"
-# CentOS 7.2 1602 from
-# https://aws.amazon.com/marketplace/fulfillment?productId=b7ee8a69-ee97-4a49-9e68-afaee216db2e&ref_=dtl_psb_continue&region=us-east-1
-# (CentOS doesn't currently publish an AWS 7.3 image. https://bugs.centos.org/view.php?id=12448)
-amis = {
-  us-east-1 = "ami-6d1c2007"
-  us-east-2 = "ami-6a2d760f"
-  us-west-1 = "ami-af4333cf"
-  us-west-2 = "ami-d2c924b2"
-}
-instance_type = "t2.micro"
--- dev/worker/vars.tf
+++ dev/worker/vars.tf
@@ -1,5 +1,19 @@
-variable "key_name" {}
-variable "amis" {
-  type = "map"
+variable "key_name" {
+  default = "donkey"
 }
-variable "instance_type" {}
+
+# CentOS 7.2 1602 from
+# https://aws.amazon.com/marketplace/fulfillment?productId=b7ee8a69-ee97-4a49-9e68-afaee216db2e&ref_=dtl_psb_continue&region=us-east-1
+# (CentOS doesn't currently publish an AWS 7.3 image. https://bugs.centos.org/view.php?id=12448)
+variable "amis" {
+  default = {
+    us-east-1 = "ami-6d1c2007"
+    us-east-2 = "ami-6a2d760f"
+    us-west-1 = "ami-af4333cf"
+    us-west-2 = "ami-d2c924b2"
+  }
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
