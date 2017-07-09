bd0fc7b16dd1edcdb7c9bbc737e9c72f01e79cba
Tyler Roscoe <tyler@cryptio.net>
2017-07-09 16:28:22 -0600

    Delete base/ and worker/.

--- modules/worker/outputs.tf
+++
@@ -1,7 +0,0 @@
-# Connecting through the elastic ip serves as a test that the instance <-> eip
-# connection is working.
-output "public_ip" {
-  value = "${aws_instance.main.public_ip}"
-  # See note in instance.tf.
-  # value = "${aws_eip.main_instance.public_ip}"
-}
